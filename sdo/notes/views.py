74918d7df0a7c57803a50eb85696234885fd5969
Юрий Лылов <lylovY@yandex.ru>
2023-02-08 14:19:57 +0300

    Notes 4

--- sdo/notes/views.py
+++ sdo/notes/views.py
@@ -16,7 +16,9 @@ class NoteList(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['username'] = self.kwargs.get('username')
+        username = self.kwargs.get('username')
+        context['username'] = username
+        context['note_count'] = Note.objects.filter(user__username=username).count()
         return context
 
     def get_queryset(self):
